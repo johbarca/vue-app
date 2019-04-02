00a28145ed4e4ee8591eb75f78528b192cf05b46
barca <330492740@qq.com>
2019-04-02 12:25:07 +0800

    代码格式化

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-new */
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
@@ -5,7 +6,9 @@ import App from './App'
 import router from './router'
 
 import fontawesome from '@fortawesome/fontawesome'
-import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+import {
+  FontAwesomeIcon
+} from '@fortawesome/vue-fontawesome'
 import solid from '@fortawesome/fontawesome-free-solid'
 import regular from '@fortawesome/fontawesome-free-regular'
 import brands from '@fortawesome/fontawesome-free-brands'
@@ -24,8 +27,8 @@ Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false
 
 Vue.use(ElementUI, {
-    size: 'small'
-  })
+  size: 'small'
+})
 
 new Vue({
   el: '#app',
